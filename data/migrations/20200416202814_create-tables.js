3c3b76f12073bd3049d48a40667efe0215d754da
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-04-16 21:22:12 -0400

    Wrote migration file that creates all the tables

---
+++ data/migrations/20200416202814_create-tables.js
@@ -0,0 +1,46 @@
+exports.up = function (knex) {
+	return knex.schema
+		.createTable("ingredients", tbl => {
+			tbl.increments();
+			tbl.string("ingredient_name", 128).notNullable();
+		})
+		.createTable("recipes", tbl => {
+			tbl.increments();
+			tbl.string("recipe_name", 128).notNullable();
+		})
+		.createTable("steps", tbl => {
+			tbl
+				.integer("recipe_id")
+				.unsigned()
+				.notNullable()
+				.references("recipes.id");
+			tbl.float("step_number");
+			tbl.string("instructions").notNullable();
+		})
+		.createTable("recipe_ingredients_steps", tbl => {
+			tbl.primary(["recipe_id", "ingredient_id"]);
+			tbl
+				.integer("recipe_id")
+				.unsigned()
+				.notNullable()
+				.references("recipes.id");
+			tbl
+				.integer("ingredient_id")
+				.unsigned()
+				.notNullable()
+				.references("ingredients.id");
+			tbl
+				.integer("step_number")
+				.unsigned()
+				.notNullable()
+				.references("steps.step_number");
+		});
+};
+
+exports.down = function (knex) {
+	return knex.schema
+		.dropTableIfExists("recipe_ingredients_steps")
+		.dropTableIfExists("steps")
+		.dropTableIfExists("recipes")
+		.dropTableIfExists("ingredients");
+};
